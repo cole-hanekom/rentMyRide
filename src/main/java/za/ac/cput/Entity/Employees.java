669857f7401cf74a511512fa58f1bcf096fc98ff
TyronneLloyd <215141210@mycput.ac.za>
2021-05-28 23:28:42 +0200

    Initial commit for Assignment 3 rentMyRide

---
+++ src/main/java/za/ac/cput/Entity/Employees.java
@@ -0,0 +1,53 @@
+package za.ac.cput.Entity;
+
+public class Employees {
+    private String emp_Num, firstName, surname, login_Credentials;
+
+    private Employees(Builder builder){
+        this.emp_Num = builder.emp_Num;
+        this.firstName = builder.firstName;
+        this.surname = builder.surname;
+        this.login_Credentials = builder.login_Credentials;
+    }
+    @Override
+    public String toString() {
+        return "Builder{" +
+                "emp_Num='" + emp_Num + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", surname='" + surname + '\'' +
+                ", login_Credentials='" + login_Credentials + '\'' +
+                '}';
+    }
+    public static class Builder{
+        private String emp_Num, firstName, surname, login_Credentials;
+
+        public Builder setEmp_Num(String emp_Num){
+            this.emp_Num = emp_Num;
+            return this;
+        }
+        public Builder setFirstName(String firstName){
+        this.firstName = firstName;
+        return  this;
+        }
+        public Builder setSurname(String surname){
+        this.surname = surname;
+        return this;
+        }
+        public Builder setLogin_Credentials(String login_Credentials){
+        this.login_Credentials = login_Credentials;
+        return this;
+        }
+        public Employees build(){
+            return new Employees( this);
+        }
+
+       public Builder copy(Employees employee){
+            this.emp_Num = employee.emp_Num;
+            this.firstName = employee.firstName;
+            this.surname = employee.surname;
+            this.login_Credentials = employee.login_Credentials;
+            return this;
+       }
+
+    }//inner class
+}//class
